4c2904c673e061d48d2d83e3f3168d06e28303fa
zhxyxxx <zhxyxxx@yahoo.co.jp>
2019-12-17 11:42:12 +0900

    controller file maked

---
+++ app/controllers/MotherListController.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+/**
+  * 親リストのコントローラ
+  */
+@Singleton
+class MotherListController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+
+}
