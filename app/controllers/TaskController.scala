ef345bb38f8c1379dd7774f984cfe7ee24af280d
Nanami Mikiya <unison.0412@gmail.com>
2019-12-17 11:53:43 +0900

    タスク一覧とタスク追加をControllerに追加

--- app/controllers/TaskController.scala
+++ app/controllers/TaskController.scala
@@ -1,5 +1,41 @@
 package controllers
 
-class TaskController {
+import javax.inject.{Inject, Singleton}
+import models.{Task, Tasks, List, Lists}
+import play.api.mvc.{AbstractController, ControllerComponents}
 
+@Singleton
+class TaskController @Inject()(tasks: Tasks)(lists: Lists)(cc: ControllerComponents) extends AbstractController(cc) {
+
+  /**
+    * インデックスページを表示
+    */
+  def list(listId: Int) = Action { request =>
+    val entries  = tasks.listFromListID(listId)
+    Ok(views.html.list(entries))
+  }
+
+  def register(listId: Int) = Action { request =>
+    val genreId = lists.getFromListID.get.genre_id //要変更
+    Ok(views.html.taskForm(listId, genreId))
+  }
+
+  def confirm(listId: Int) = Action { request =>
+    (for {
+      param       <- request.body.asFormUrlEncoded
+      name1       <- param.get("name1").flatMap(_.headOption)
+      name2       <- param.get("name2").flatMap(_.headOption)
+      description <- param.get("description").flatMap(_.headOption)
+      deadline       <- param.get("deadline").flatMap(_.headOption)
+    } yield {
+      tasks.save(Task(listId, name1, name2, description, deadline, false))
+      Redirect("./lists/" + listId).withNewSession
+    }).getOrElse[Result](Redirect("./lists/" + listId + "/add"))
+  }
+
+  def comp = TODO
+
+  def edit = TODO
+
+  def delete = TODO
 }
--- app/models/Tasks.scala
+++ app/models/Tasks.scala
@@ -21,7 +21,28 @@ class Tasks @Inject()(dbcp: DBConfigProvider)(implicit ec: ExecutionContext) ext
     * @return
     */
   def list: Seq[Task] = Await.result(
-    db.run(sql"SELECT taskId, listId, name1, name2, description, deadline, createdAt, isDone FROM #$table".as[Task])
+    db.run(sql"SELECT task_id, list_id, name1, name2, description, deadline, created_it, is_done FROM #$table".as[Task])
   )
 
+  /**
+    * 親リストのIDを引数にとり、親リストIDが一致するタスクのみを取得する
+    * @param listId
+    * @return
+    */
+  def listFromListID(listId: Int): Seq[Task] = Await.result(
+    db.run(sql"SELECT task_id, list_id, name1, name2, description, deadline, created_it, is_done FROM #$table WHERE list_id='#$listId'".as[Task])
+  )
+
+  def save(task: Task): Int = task match {
+    case Task(0, listId, name1, name2, description, deadline, _, isDone) =>
+      Await.result(
+        db.run(sqlu"INSERT INTO #$table (listId, name1, name2,  description, deadline, is_done) VALUES ('#$listId', '#$name1', '#$name2', '#$description', '#$deadline', '#$isDone')")
+      )
+    case Task(taskId, listId, name1, name2, description, deadline, _, isDone) =>
+      Await.result(
+        db.run(
+          sqlu"UPDATE #$table SET name1='#$name1', name2='#$name2', description='#$description', deadline='#$deadline', is_done='#$isDone' WHERE task_id = #$taskId"
+        )
+      )
+  }
 }
