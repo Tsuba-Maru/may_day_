eac40af1a8c4ca4de04aab261eaf705cdf55c414
zhxyxxx <41413243+zhxyxxx@users.noreply.github.com>
2020-01-27 15:43:24 +0900

    tasks 締切修正

--- app/models/Tasks.scala
+++ app/models/Tasks.scala
@@ -21,7 +21,10 @@ class Tasks @Inject()(dbcp: DBConfigProvider)(implicit ec: ExecutionContext) ext
     * @return
     */
   def list: Seq[Task] = Await.result(
-    db.run(sql"SELECT task_id, list_id, name1, name2, description, deadline, created_at, is_done FROM #$table".as[Task])
+    db.run(
+      sql"SELECT task_id, list_id, name1, name2, description, dead_year, dead_month, dead_day, created_at, is_done FROM #$table"
+        .as[Task]
+    )
   )
 
   /**
@@ -33,19 +36,29 @@ class Tasks @Inject()(dbcp: DBConfigProvider)(implicit ec: ExecutionContext) ext
     db.run(sql"SELECT * FROM #$table WHERE list_id='#$listId'".as[Task])
   )
 
-  def findByID(listId: Int): Option[Task] = Await.result(
-    db.run(sql"SELECT * FROM #$table WHERE list_id=#$listId".as[Task].headOption)
+  def findByID(taskId: Int): Option[Task] = Await.result(
+    db.run(sql"SELECT * FROM #$table WHERE task_id='#$taskId'".as[Task].headOption)
+  )
+
+  def findByIsDone(listId: Int, isDone: Boolean): Seq[Task] = Await.result(
+    db.run(sql"SELECT * FROM #$table WHERE list_id='#$listId' AND is_done='#$isDone'".as[Task])
+  )
+
+  def search(listId: Int, title: String, isDone: Boolean): Seq[Task] = Await.result(
+    db.run(sql"SELECT * FROM #$table WHERE list_id='#$listId' AND name1='#$title' AND is_done='#$isDone'".as[Task])
   )
 
   def save(task: Task): Int = task match {
-    case Task(0, listId, name1, name2, description, deadline, _, isDone) =>
-      Await.result(
-        db.run(sqlu"INSERT INTO #$table (listId, name1, name2,  description, deadline, is_done) VALUES ('#$listId', '#$name1', '#$name2', '#$description', '#$deadline', '#$isDone')")
-      )
-    case Task(taskId, listId, name1, name2, description, deadline, _, isDone) =>
+    case Task(0, listId, name1, name2, description, deadYear, deadMonth, deadDay, _, isDone) =>
       Await.result(
         db.run(
-          sqlu"UPDATE #$table SET name1='#$name1', name2='#$name2', description='#$description', deadline='#$deadline', is_done='#$isDone' WHERE task_id = #$taskId"
+          sqlu"INSERT INTO #$table (list_id, name1, name2,  description, dead_year, dead_month, dead_day, is_done) VALUES ('#$listId', '#$name1', '#$name2', '#$description', '#$deadYear', '#$deadMonth', '#$deadDay', '#$isDone')"
+        )
+      )
+    case Task(taskId, listId, name1, name2, description, deadYear, deadMonth, deadDay, _, isDone) =>
+      Await.result(
+        db.run(
+          sqlu"UPDATE #$table SET name1='#$name1', name2='#$name2', description='#$description', dead_year='#$deadYear', dead_month='#$deadMonth', dead_day='#$deadDay', is_done='#$isDone' WHERE task_id = '#$taskId'"
         )
       )
   }
@@ -58,12 +71,12 @@ class Tasks @Inject()(dbcp: DBConfigProvider)(implicit ec: ExecutionContext) ext
     */
   def switchIsDone(taskId: Int, isDone: Boolean): Int = Await.result(
     db.run(
-      sqlu"UPDATE #$table SET is_done='#$isDone' WHERE task_id = #$taskId"
+      sqlu"UPDATE #$table SET is_done='#$isDone' WHERE task_id = '#$taskId'"
     )
   )
 
   def delete(taskId: Int): Int = Await.result(
-    db.run(sqlu"DELETE FROM #$table WHERE task_id = #$taskId")
+    db.run(sqlu"DELETE FROM #$table WHERE task_id = '#$taskId'")
   )
  
    def deleteList(id: Int): Int = Await.result(
